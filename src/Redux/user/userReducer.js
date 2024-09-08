9e4cf06dbca4983ce8c299ef6443b8ca4cbbfb02
Erickkipkoech <cheruiyotkipkoech612@gmail.com>
2024-09-08 21:31:52 +0300

    done 1st

---
+++ src/Redux/user/userReducer.js
@@ -0,0 +1,34 @@
+import { GET_USERS_FAILURE, GET_USERS_SUCCESS, GET_USERS_REQUEST } from "./userTypes";
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_USERS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case GET_USERS_SUCCESS:
+            return {
+                loading: false,
+                users: action.payload,
+                error: ''
+            }
+        case GET_USERS_FAILURE:
+            return{
+                loading: false,
+                users:[],
+                error:action.payload
+            }
+        default:
+            return state;
+    }
+   
+}
+
+export default reducer;
